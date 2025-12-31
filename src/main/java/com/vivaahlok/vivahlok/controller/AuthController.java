e65ea715482d39632fbe723e0bce2bac646f4483
abh1102 <abhishekjha2507@gmail.com>
2025-12-31 15:18:06 +0530

    fix the booking api

--- src/main/java/com/vivaahlok/vivahlok/controller/AuthController.java
+++ src/main/java/com/vivaahlok/vivahlok/controller/AuthController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 @Tag(name = "Authentication", description = "Authentication APIs")
 public class AuthController {
